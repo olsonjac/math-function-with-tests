13f5c7c8c33eedf693b0d16e2fb09e0365d4d53e
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2023-03-06 11:36:49 -0500

    Create math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Create a function to add numbers together
+def add(x,y):
+    return x + y
+
+#Create a function to subtract numbers
+def subtract(x,y):
+    return x - y
+
+#Create a function to multiply numbers
+def multiply(x,y):
+    return x * y
+
+#Create a function to capitalize a string
+def capitalize(string):
+    return string.capitalize()
